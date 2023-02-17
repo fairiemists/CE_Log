a7329a006a6374494df8f64c47ed079ebb0b7360
fairiemists <v.deweyness@gmail.com>
2023-02-16 17:00:27 -0800

    Updated Project URLs + Created First View Function

--- backend/ce_log/views.py
+++ backend/ce_log/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Credit
+from .serializers import CreditSerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_credits(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+
+    return Response('ok')
+
