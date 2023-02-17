3bbdc52e85186502c3c5b30034723fc17b73770a
fairiemists <v.deweyness@gmail.com>
2023-02-16 17:07:33 -0800

    Updated User Model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,8 +1,13 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+
+    ptin = models.CharField(max_length=10, null=True)
+    is_manager = models.BooleanField(default=False)
+    manager_id = models.IntegerField(null=True)
+
     pass
     '''
     This is a custom version of the built in User class
